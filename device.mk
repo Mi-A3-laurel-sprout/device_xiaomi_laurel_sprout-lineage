833c6674f4d5257b5286090eabf9f001f6841870
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    laurel_sprout: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -93,8 +93,7 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
+    vendor.qti.hardware.memtrack-service \
     vendor.qti.hardware.display.allocator-service
 
 PRODUCT_PACKAGES += \
@@ -103,8 +102,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     gralloc.trinket \
-    hwcomposer.trinket \
-    memtrack.trinket
+    hwcomposer.trinket
 
 # DRM
 PRODUCT_PACKAGES += \
