153aa92a0b18dc7354e344f2276fea4d5ec586dc
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-10-17 21:04:14 +0000

    laurel_sprout: Implement udfps handler
    Based on: https://github.com/LineageOS/android_device_xiaomi_sm8250-common/blob/lineage-21/udfps/UdfpsHandler.cpp
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -110,7 +110,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.xiaomi
+    android.hardware.biometrics.fingerprint-service.xiaomi \
+    libudfpshandler
 
 # FM
 PRODUCT_PACKAGES += \
